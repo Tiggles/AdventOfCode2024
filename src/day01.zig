3259ed29e348ef1065f41c22ec34c21000ad9094
Jonas Malte Hinchely <jonas@hinchely.dk>
2023-12-03 12:17:06 +0100

    Up to day 3 (in js)

--- src/day01.zig
+++ src/day01.zig
@@ -11,7 +11,10 @@ const gpa = util.gpa;
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    
+    var sum = 0;
+    for (splitAny(u8, data, "\n")) |line| {
+        std.debug.print("{}\n", .{line});
+    }
 }
 
 // Useful stdlib functions
@@ -44,3 +47,62 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+// function test1(input) {
+//     let sum = 0;
+//     for (const line of input.split("\n")) {
+//         let first = "";
+//         let last = "";
+//         for (const char of line) {
+//             if (numbersAsNumericals.includes(char)) {
+//                 if (first !== "") last = char;
+//                 else {
+//                     first = char;
+//                     last = char;
+//                 }
+//             }
+//         }
+//
+//         sum += parseInt(first + last, 10);
+//     }
+//     console.log(sum);
+// }
+//
+// const numbersAsNumericals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// const numbersAsWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+//
+// function test2(input) {
+//     let sum = 0;
+//     for (const line of input.split("\n")) {
+//         let first = "";
+//         let last = "";
+//         let index = 0;
+//         for (const char of line.trim()) {
+//             if (numbersAsNumericals.includes(char)) {
+//                 if (first !== "") last = char;
+//                 else {
+//                     first = char;
+//                     last = char;
+//                 }
+//             } else {
+//                 const remainingLine = line.slice(index);
+//                 for (const [idx, word] of numbersAsWords.entries()) {
+//                     if (remainingLine.startsWith(word)) {
+//                         if (first !== "") last = `${idx}`;
+//                         else {
+//                             first = `${idx}`;
+//                             last = `${idx}`;
+//                         }
+//                         break;
+//                     }
+//                 }
+//             }
+//             index += 1;
+//         }
+//
+//         const result = parseInt(first + last, 10);
+//         console.log(`Parsing ${first} and ${last} as ${result}, full line: ${line}`);
+//         sum += result;
+//     }
+//     console.log(sum);
+// }
